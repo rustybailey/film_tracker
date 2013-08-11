25c6b40eeb9223ac43abe30f4542f1e5c5c34440
Rusty Bailey <rustylbailey@gmail.com>
2013-08-11 16:40:25 -0400

    Add pie chart for runtime in minutes.

--- js/movies_watched.js
+++ js/movies_watched.js
@@ -53,9 +53,60 @@ $(document).ready(function() {
                         if(movieIds.length - 1 === i) {
                             console.log("Finished in " + count + " seconds.");
                             clearTimeout(timer);
-                            console.log(totalMinutesWatched());
-                            var minutesYearToDate = (d3.time.dayOfYear(new Date()) + 1) * 24 * 60
-                            console.log((totalMinutesWatched() / minutesYearToDate) * 100)
+                            var runtime = totalMinutesWatched();
+                            var minutesAwakeToDate = (d3.time.dayOfYear(new Date()) + 1) * 16 * 60;
+                            var minutesSleepingToDate = (d3.time.dayOfYear(new Date()) + 1) * 8 * 60;
+
+                            var data = [
+                                ["Sleeping", minutesSleepingToDate],
+                                ["Watching Movies", runtime],
+                                ["Rest of Day", minutesAwakeToDate - runtime]
+                            ];
+
+                            var tooltip = d3.select("body")
+                                .append("div")
+                                .attr('class', 'pie-tooltip')
+                                .style("position", "absolute")
+                                .style("z-index", "10")
+                                .style("visibility", "hidden")
+                                .text("a simple tooltip");
+
+                            var width = 200,
+                                height = 200,
+                                radius = Math.min(width, height) / 2;
+
+                            var color = d3.scale.ordinal()
+                                .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+                            var arc = d3.svg.arc()
+                                .outerRadius(radius - 10)
+                                .innerRadius(0);
+
+                            var pie = d3.layout.pie()
+                                .sort(null)
+                                .value(function(d) { return d[1]; });
+
+                            var svg = d3.select(".runtime-pie").append("svg")
+                                .attr("width", width)
+                                .attr("height", height)
+                              .append("g")
+                                .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+                            var g = svg.selectAll(".arc")
+                              .data(pie(data))
+                            .enter().append("g")
+                              .attr("class", "arc");
+
+                            g.append("path")
+                                .attr("d", arc)
+                                .style("fill", function(d) { return color(d.data[0]); })
+                                .on("mouseover", function(d){
+                                    var percent = (d.endAngle - d.startAngle) / (2 * Math.PI);
+                                    percent = d3.format('%')(percent); 
+                                    return tooltip.style("visibility", "visible").text(d.data[0] + " (" + percent + ")");
+                                })
+                                .on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
+                                .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
                         }
                     }
                 });
@@ -78,7 +129,9 @@ $(document).ready(function() {
             $(this).text("Hide Posters");
         }
         $('#movies-container').slideToggle();
-    })
+    });
+
+
 
 });
 
