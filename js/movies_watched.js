948d03ec60d43f07afe2b4a72ac88569b4cef541
Rusty Bailey <rustylbailey@gmail.com>
2013-08-11 15:27:20 -0400

    Query data for each movie id

--- js/movies_watched.js
+++ js/movies_watched.js
@@ -13,31 +13,50 @@ $(document).ready(function() {
             "original"
         ];
 
-    var movieTemplate = _.template('<div id="<%= movieName %>" data-year="<%= year %>" class="movie">' +
+    var movieTemplate = _.template('<div id="<%= movieId %>" data-year="<%= year %>" class="movie">' +
         '<img src="<%= imgLink %>">' +
         '<div class="title-card"><%= movieName %> (<%= year %>)</div>' +
         '</div>'
         );
 
+    var count = 0;
+    var timer = setInterval(function(){count++}, 1000);
+
     $.ajax({
         type: 'GET',
         url: url + list_mode + question_key,
-        async: false,
-        contentType: 'application/json',
         dataType: 'jsonp',
-        success: function(json) {
-            $.each(json.items, function(index, movie){
+        success: function(data) {
+            var movieIds = [];
+            $.each(data.items, function(index, movie){
                 if(!movie) return // Fix for movies that return null
                 var release = movie.release_date;
                 var year = new Date(release).getFullYear();
                 var title = movie.title;
+                var id = movie.id;
                 var imgLink = base_url + poster_sizes[0] + movie.poster_path;
                 $(movieTemplate({
+                    movieId: id,
                     movieName: title,
                     year: year,
                     imgLink: imgLink
                 })).appendTo('#movies-container');
-            })
+                movieIds.push(id);
+            });
+            $.each(movieIds, function(i, v) {
+                $.ajax({
+                    type: 'GET',
+                    url: url + 'movie/' + v + question_key,
+                    dataType: 'jsonp',
+                    success: function(movie) {
+                        $('#' + v).data(movie);
+                        if(movieIds.length - 1 === i) {
+                            console.log("Finished in " + count + " seconds.");
+                            clearTimeout(timer);
+                        }
+                    }
+                });
+            });
         }
     });
 
